d37c19e8ff37dc38552cb86c0a7b5c383bce1196
naderZ2 <{ID}+{username}@users.noreply.github.com>
2025-04-12 16:37:51 +0200

    download files

--- js/scripts.js
+++ js/scripts.js
@@ -15,3 +15,31 @@ document.addEventListener("DOMContentLoaded", () => {
   window.addEventListener('load', () => {
     document.querySelector('.content').style.display = 'block';
   });
+
+
+
+  document.getElementById("download-link-profile").addEventListener("click", function () {
+    // const link = document.createElement("a");
+    // link.href = "assets/pdf/El-Sewedy-Power-Cables-profile-2025.pdf";
+    // link.download = "ELSEWDYPOWER-CATALOGE.pdf";
+    // document.body.appendChild(link);
+    // link.click();
+    // document.body.removeChild(link);
+
+    window.open("assets/pdf/El-Sewedy-Power-Cables-profile-2025.pdf", "_blank");
+    
+    
+  });
+  
+  
+  document.getElementById("download-link-CATALOGE").addEventListener("click", function () {
+    // const link = document.createElement("a");
+    // link.href = "assets/pdf/ELSEWDYPOWER-CATALOGE.pdf";
+    // link.download = "ELSEWDYPOWER-CATALOGE.pdf";
+    // document.body.appendChild(link);
+    // link.click();
+    // document.body.removeChild(link);
+    window.open("assets/pdf/ELSEWDYPOWER-CATALOGE.pdf", "_blank");
+  });
+
+
